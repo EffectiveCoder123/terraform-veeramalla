25e1db8386cb3356f787c9523ccb0ecc3ff5cc07
EffectiveCoder123 <161019495+EffectiveCoder123@users.noreply.github.com>
2024-05-07 15:26:53 +0000

    added ec2 micro instance

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -1,8 +1,10 @@
 provider "aws" {
-    region = "us-east-1"  # Set your desired AWS region
+    region = "us-west-1"  # Set your desired AWS region
 }
 
-resource "aws_instance" "example" {
-    ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
+resource "aws_instance" "tflearning1" {
+    ami           = "ami-036cafe742923b3d9"  # Specify an appropriate AMI ID
     instance_type = "t2.micro"
+    subnet_id = "subnet-0d61de263d211d6de"
+    key_name = "ubuntu-awsdevops-1"
 }
